3dcd2620573ac7f94f19667a017d00a3bfd740f2
jamiedownsworth <3920244+jamiedownsworth@users.noreply.github.com>
2025-09-15 19:45:02 +0100

    fix: always display six guesses even if fewer were recorded

--- src/helpers/scoreToEmoji.ts
+++ src/helpers/scoreToEmoji.ts
@@ -2,12 +2,17 @@ import { GuessType } from "../types/guess";
 import { emojis } from "../constants";
 
 export function scoreToEmoji(guesses: GuessType[]): string {
-  return guesses
+  const result = guesses
     .map((guess) =>
       guess.isCorrect === true ? emojis.correct
         : guess.skipped === true ? emojis.skip
         : guess.isCorrect === false ? emojis.incorrect
         : emojis.empty
-    )
-    .join('');
+    );
+
+  for (let i = 0; i < 6 - guesses.length; i++) {
+    result.push(emojis.empty);
+  }
+
+  return result.join('');
 }
