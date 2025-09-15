41d897b4759f5126534b96eb027550128a11da76
jamiedownsworth <3920244+jamiedownsworth@users.noreply.github.com>
2025-09-15 19:36:23 +0100

    fix: better sorting function, preventing searching for 'king gizzard' showing the correct result

--- src/helpers/searchSong.ts
+++ src/helpers/searchSong.ts
@@ -7,14 +7,25 @@ export function searchSong(searchTerm: string): Song[] {
   return songs
     .filter((song: Song) => {
       const songName = song.name.toLowerCase();
-      const songArtist = song.artist.toLowerCase();
+      //const songArtist = song.artist.toLowerCase();
 
-      if (songArtist.includes(searchTerm) || songName.includes(searchTerm)) {
+      if (
+        //songArtist.includes(searchTerm) ||
+        songName.includes(searchTerm)
+      ) {
         return song;
       }
     })
-    .sort((a, b) =>
-      a.artist.toLowerCase().localeCompare(b.artist.toLocaleLowerCase())
-    )
+    .sort((a :Song, b :Song) =>{
+      const opts = { sensitivity: "base", numeric: true };
+
+      const nameCompare = a.name.localeCompare(b.name, undefined, opts);
+      if (nameCompare !== 0) return nameCompare;
+
+      const artistCompare = a.artist.localeCompare(b.artist, undefined, opts);
+      if (artistCompare !== 0) return artistCompare;
+
+      return a.youtubeId.localeCompare(b.youtubeId, undefined, opts);
+    })
     .slice(0, 5);
 }
