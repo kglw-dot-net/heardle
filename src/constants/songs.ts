3537a91afc77ff04ad7b249413d06b0ea2df529a
Jamie Downsworth <3920244+jamiedownsworth@users.noreply.github.com>
2026-05-09 01:13:35 +0100

    fix: replace another blocked YT id

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -242,7 +242,7 @@ export const songs :Song[] = [
   { artist: "King Gizzard and the Lizard Wizard", name: "Evil Man", youtubeId: "_UYcKce8fg0" },
   { artist: "King Gizzard and the Lizard Wizard with Mild High Club", name: "D-Day", youtubeId: "ybcKrnivzT8" },
   { artist: "King Gizzard and the Lizard Wizard", name: "Ataraxia", youtubeId: "t23If5O_AdI" },
-  { artist: "King Gizzard and the Lizard Wizard", name: "Drum Run", youtubeId: "6HOs7B3YxMzfqb02" },
+  { artist: "King Gizzard and the Lizard Wizard", name: "Drum Run", youtubeId: "_Iqb--ZhJJ0" },
   { artist: "Tropical Fuck Storm/King Gizzard and the Lizard Wizard", name: "Satanic Slumber Party Part 3 (Hoof And Horn)", youtubeId: "L_8AJcXH_Fk" },
   { artist: "King Gizzard and the Lizard Wizard", name: "Nuclear Fusion", youtubeId: "4MFVhqcRpN4" },
   { artist: "King Gizzard and the Lizard Wizard", name: "Astroturf", youtubeId: "ql_z7t6WGPg" },
