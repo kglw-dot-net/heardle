12eb3d88a6aa862e050bef71c1d2e8720cfa3c29
Jamie Downsworth <3920244+jamiedownsworth@users.noreply.github.com>
2026-07-05 02:14:08 +0100

    fix: another broken song id

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -299,5 +299,5 @@ export const songs :Song[] = [
   { artist: "King Gizzard and the Lizard Wizard", name: "Anoxia", youtubeId: "wpDkWFCkf9g" },
   { artist: "King Gizzard and the Lizard Wizard", name: "All Is Known", youtubeId: "CxlUl6dVxPk" },
   { artist: "King Gizzard and the Lizard Wizard", name: "Garage Liddiard", youtubeId: "kRhUoO0nqfs" },
-  { artist: "King Gizzard and the Lizard Wizard", name: "Float Along – Fill Your Lungs", youtubeId: "ZzVf5P3FlCk", slug: "float-along-fill-your-lungs-2" }
+  { artist: "King Gizzard and the Lizard Wizard", name: "Float Along – Fill Your Lungs", youtubeId: "VXQGIp_rMds", slug: "float-along-fill-your-lungs-2" }
 ];
